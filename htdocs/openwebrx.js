510cc39b59ad9dde72d0c92abdc22f106b8935bb
J.W. Jagersma <jwjagersma@gmail.com>
2020-08-31 01:20:18 +0200

    add dropdown box to select grid snap size

--- htdocs/openwebrx.js
+++ htdocs/openwebrx.js
@@ -583,7 +583,9 @@ function canvas_mouseup(evt) {
     var relativeX = get_relative_x(evt);
 
     if (!canvas_drag) {
-        $('#openwebrx-panel-receiver').demodulatorPanel().getDemodulator().set_offset_frequency(Math.round(canvas_get_freq_offset(relativeX)/100)*100);
+        var step = $('#webrx-mouse-step option:selected').val();
+        var freq = Math.round(canvas_get_freq_offset(relativeX) / step) * step;
+        $('#openwebrx-panel-receiver').demodulatorPanel().getDemodulator().set_offset_frequency(freq);
     }
     else {
         canvas_end_drag();
